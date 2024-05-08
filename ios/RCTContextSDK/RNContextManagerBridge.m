0a71ab84bd4efda273189815ccd15c6bb13d489e
irgendeinich <reinhard@contextsdk.com>
2024-05-08 10:49:14 +0200

    fresh start

--- ios/RCTContextSDK/RNContextManagerBridge.m
+++
@@ -1,8 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(RNContextManager, NSObject)
-
-RCT_EXTERN_METHOD(setup:(NSString *)licenseKey resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(sdkVersionWithResolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
-
-@end
